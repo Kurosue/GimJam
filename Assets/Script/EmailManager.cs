8e7a1a257eb746bf49afbae964d49cc2927d5d9e
Kurosue <adityarahmadeni30@gmail.com>
2024-01-30 22:11:58 +0700

    Sistem Email

---
+++ Assets/Script/EmailManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Import TextMeshPro namespace
+
+public class InboxManager : MonoBehaviour
+{
+    public List<EmailMessage> allMessages;
+    private List<EmailMessage> availableMessages = new List<EmailMessage>();
+
+    public TextMeshProUGUI subjectText; // Change from Text to TextMeshProUGUI
+    public TextMeshProUGUI bodyText;    // Change from Text to TextMeshProUGUI
+
+    private void Start()
+    {
+        InitializeAvailableMessages();
+        ShowRandomMessage();
+    }
+
+    void InitializeAvailableMessages()
+    {
+        availableMessages.AddRange(allMessages);
+    }
+
+    void ShowRandomMessage()
+    {
+        if (availableMessages.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availableMessages.Count);
+            EmailMessage currentMessage = availableMessages[randomIndex];
+
+            // Display the message
+            subjectText.text = currentMessage.subject;
+            bodyText.text = currentMessage.body;
+
+            // Remove the displayed message from the available messages
+            availableMessages.RemoveAt(randomIndex);
+        }
+        else
+        {
+            Debug.Log("No more messages");
+        }
+    }
+
+    public void AcceptEmail()
+    {
+        ShowRandomMessage();
+    }
+
+    public void DeclineEmail()
+    {
+        // Handle declining the email
+        ShowRandomMessage();
+    }
+}
---
+++ Assets/Script/EmailMessage.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Email Message", menuName = "Custom/Email Message")]
+public class EmailMessage : ScriptableObject
+{
+    public string subject;
+    [TextArea(3, 10)]
+    public string body;
+}
