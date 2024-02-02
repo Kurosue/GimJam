00cec10615ec9a1288cef382f9c99e03b4598e56
Hazim <16523226@mahasiswa.itb.ac.id>
2024-02-02 15:07:33 +0700

    Reapply "akdshlajshf"
    This reverts commit 778f719eec424a76f995c857fe09de4e8a5ca355.

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -6,29 +6,16 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
-    [SerializeField] GameObject[] objek;
-    [SerializeField] objdrag[] Objdrag = new objdrag[4];
 
-    void Awake() {
-        for (int i = 0; i < objek.Length; i++){
-            Objdrag[i] = objek[i].GetComponent<objdrag>();
-        }
-    }
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
-        for (int i = 0; i <objek.Length; i++){
-            Objdrag[i].enabled = false;
-        }
     }
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
-        for (int i = 0; i <objek.Length; i++){
-            Objdrag[i].enabled = true;
-        }
     }
     public void Settings()
     {
