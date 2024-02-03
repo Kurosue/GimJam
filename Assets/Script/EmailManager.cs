2ff88ebe23dc32d49fa210a5f8cbf0bea70e04d8
Kurond <68522463+Kurosue@users.noreply.github.com>
2024-02-03 18:41:01 +0700

    Update EmailManager.cs

--- Assets/Script/EmailManager.cs
+++ Assets/Script/EmailManager.cs
@@ -114,19 +114,6 @@ public class InboxManager : MonoBehaviour
         }
     }
 
-    public static void Shuffle<T>(this IList<T> list)
-    {
-        int n = list.Count;
-        while (n > 1)
-        {
-            n--;
-            int k = random.Next(n + 1);
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
-        }
-    }
-
     public void ResetGame()
     {
         _trigger.jumlahbroto = 0;
